979aba8839f5f0a2b4f81d8f4c16ddc5fa2ee149
Michele Yoshida <68092286+MicheleYoshi@users.noreply.github.com>
2020-09-17 11:17:28 -0700

    Updates

---
+++ precache-manifest.a4c8a8dd306cc42acdf900f4ce157bc0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d0e5739f375d1a44eb38db7acf312f",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0669082eb2d07820299e",
+    "url": "/react-portfolio/static/css/2.a25b08ee.chunk.css"
+  },
+  {
+    "revision": "c530d5150eea05b93418",
+    "url": "/react-portfolio/static/css/main.1e81692a.chunk.css"
+  },
+  {
+    "revision": "0669082eb2d07820299e",
+    "url": "/react-portfolio/static/js/2.fcf22007.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-portfolio/static/js/2.fcf22007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c530d5150eea05b93418",
+    "url": "/react-portfolio/static/js/main.906fb7b3.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5dc55d94edeb1083dd389105c7107187",
+    "url": "/react-portfolio/static/media/yoshida-alley-img.5dc55d94.jpg"
+  }
+]);
